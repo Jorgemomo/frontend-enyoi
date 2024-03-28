20f231c7df0dbfc2cb56a12d595b7b5a20776c87
Jorgemomo <jmomo581@gmail.com>
2024-03-28 14:51:27 -0500

    Home del proyecto

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,9 +1,9 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../../auth/AuthContext";
-
 import { useNavigate, Link, Outlet } from "react-router-dom";
-
 import Permissions from "../../utils/permissions";
+
+import logo from "../../assets/images/logo192.png";
 
 const Layout = () => {
   const { signout } = useContext(AuthContext);
@@ -21,32 +21,39 @@ const Layout = () => {
 
   return (
     <div>
-      <ul className="w-full flex flex-row justify-end h-11 bg-sky-600 text-white">
+      <ul className="w-full flex flex-row justify-between items-center h-auto bg-sky-600 text-white px-20 py-2">
         <li className="mx-5 text-lg">
-          <Link to="/">Home</Link>
+          <Link to="/">
+            <img src={logo} alt="logo app" height={40} width={40} />
+          </Link>
         </li>
-
-        <Permissions>
+        <div className="flex flex-row">
           <li className="mx-5 text-lg">
-            <Link to="/dashboard">Dashboard</Link>
+            <Link to="/">Home</Link>
+          </li>
+
+          <Permissions>
+            <li className="mx-5 text-lg">
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+            <li className="mx-5 text-lg">
+              <Link to="/quotes">Citas</Link>
+            </li>
+          </Permissions>
+
+          <li className="mx-5 text-lg">
+            <Link to="/register">Registro</Link>
           </li>
           <li className="mx-5 text-lg">
-            <Link to="/quotes">Citas</Link>
+            <Link to="/login">Login</Link>
           </li>
-        </Permissions>
 
-        <li className="mx-5 text-lg">
-          <Link to="/register">Registro</Link>
-        </li>
-        <li className="mx-5 text-lg">
-          <Link to="/login">Login</Link>
-        </li>
-
-        <Permissions>
-          <li className="mx-5 text-lg">
-            <Link onClick={fnLogOut}>Salir</Link>
-          </li>
-        </Permissions>
+          <Permissions>
+            <li className="mx-5 text-lg">
+              <Link onClick={fnLogOut}>Salir</Link>
+            </li>
+          </Permissions>
+        </div>
       </ul>
 
       <Outlet />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,70 @@ import React from "react";
 
 import PrimerComponente from "../components/PrimerComponente/PrimerComponente";
 
+import logo from "../assets/images/logo192.png";
+
 const Home = () => {
   return (
-    <>
+    <div className="w-full">
       <PrimerComponente />
-    </>
+      <div className="w-full">
+        <section className="w-full flex flex-col">
+          <h2 className="mb-4 mt-11 font-bold uppercase text-center">
+            El software de citas más avanzado del mercado
+          </h2>
+          <div className="w-full flex flex-row h-auto items-center">
+            <img src={logo} alt="imagen de software" height={200} width={200} />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Repellendus corporis voluptatum labore accusamus quia magni quae,
+              quis fugiat vel delectus dicta sapiente vero officiis architecto
+              maiores esse natus aut praesentium?
+            </p>
+          </div>
+        </section>
+        <section className="w-full flex flex-col bg-sky-100">
+          <h2 className="mb-4 mt-11 font-bold uppercase text-center">
+            Revisión periódica y diagnóstico especializado
+          </h2>
+          <div className="w-full flex flex-row h-auto items-center">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Repellendus corporis voluptatum labore accusamus quia magni quae,
+              quis fugiat vel delectus dicta sapiente vero officiis architecto
+              maiores esse natus aut praesentium?
+            </p>
+            <img
+              src={logo}
+              alt="imagen de diagnóstico"
+              height={200}
+              width={200}
+            />
+          </div>
+        </section>
+        <section className="w-full flex flex-col">
+          <h2 className="mb-4 mt-11 font-bold uppercase text-center">
+            Cuidados y alimentación para tus mascotas
+          </h2>
+          <div className="w-full flex flex-row h-auto items-center">
+            <img
+              src={logo}
+              alt="imagen cuidado mascotas"
+              height={200}
+              width={200}
+            />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Repellendus corporis voluptatum labore accusamus quia magni quae,
+              quis fugiat vel delectus dicta sapiente vero officiis architecto
+              maiores esse natus aut praesentium?
+            </p>
+          </div>
+        </section>
+        <footer className="w-full flex justify-center items-center h-11 bg-sky-600 text-white ">
+          <p className="text-center">Desarrollado por: Jorge Morales </p>
+        </footer>
+      </div>
+    </div>
   );
 };
 
